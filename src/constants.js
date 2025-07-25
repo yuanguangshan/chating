987d68c4bb734de380dc975a7a8265bf7e9f939a
yuanguangshan <yuanguangshan@gmail.com>
2025-07-25 16:45:53 +0800

    修复删除消息不立刻消失的问题

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,6 @@ export const MSG_TYPE_SYSTEM = "system";
 export const MSG_TYPE_USER = "user";
 export const MSG_TYPE_HISTORY = "history";
 export const MSG_TYPE_ERROR = "error";
+export const MSG_TYPE_DELETE = "delete";
 export const MSG_TYPE_COMMAND_RESPONSE = "command_response";
 export const MSG_TYPE_GEMINI_CHAT = "gemini_chat"; // ✅ 定义常量
